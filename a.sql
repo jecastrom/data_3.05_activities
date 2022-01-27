78bc93630e05b1e633425106108c73012cb4c052
jecastrom <jcastro.de@outlook.com>
2022-01-27 01:45:03 +0100

    a

---
+++ a.sql
@@ -0,0 +1,19 @@
+SELECT
+    account_id,
+    (
+        SELECT
+            floor(avg(count(account_id)))
+    ) AS no_trans_above_avg
+FROM
+    trans
+GROUP BY
+    1
+HAVING
+    no_trans_above_avg > (
+        SELECT
+            *
+        FROM
+            avg_count_trans
+    )
+ORDER BY
+    2 DESC;
